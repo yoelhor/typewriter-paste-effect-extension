70dc71ea7a17b72ca2329e706fb178557ee3c625
Yoel Hor <yoelhor@live.com>
2023-01-22 12:48:43 +0200

    Init

---
+++ background.js
@@ -0,0 +1,31 @@
+// background.js
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: toggleDark
+    });
+});
+
+
+async function toggleDark() {
+
+    var selectElement = document.activeElement;
+
+    var i = 0;
+    var txt = await navigator.clipboard.readText();
+    var speed = 50;
+
+    if (selectElement)
+    {
+        selectElement.value = "";
+        typeWriter();
+    }
+
+    function typeWriter() {
+        if (i < txt.length) {
+            selectElement.value += txt.charAt(i);
+            i++;
+            setTimeout(typeWriter, speed);
+        }
+    }
+}
